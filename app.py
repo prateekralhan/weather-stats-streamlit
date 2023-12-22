ef39a000f46e9364e17a3024980fff46e33b1801
Prateek Ralhan <ralhanprateek@gmail.com>
2023-12-22 13:54:04 +0530

    Added "visibility" and "Air Quality Index"

--- app.py
+++ app.py
@@ -15,20 +15,23 @@ with open('style.css') as f:
 
 count = st_autorefresh(interval=5000, limit=100, key="fizzbuzzcounter")
 
-nowTime = datetime.now()
+IST = pytz.timezone('Asia/Kolkata')
+nowTime = datetime.now(IST)
 current_time = nowTime.strftime("%H:%M:%S")
 timeMetric, = st.columns(1)
 timeMetric.metric("",nowTime.strftime("%B %e, %Y"))
 
-a1, a2, a3 = st.columns(3)
+a1, a2, a3, a4 = st.columns(4)
 a1.metric("Bangalore's Temperature 🌳", f"{get_temp()}", f"{temp_difference()}"+"%")
 a2.metric("Bangalore's time 🕗", current_time)
 a3.metric("Humidity 💧", f"{get_humidity()}"+"%")
+a4.metric("AQI 🍃", f"{get_aqi()}")
 
-b1, b2, b3 = st.columns(3)
+b1, b2, b3, b4 = st.columns(4)
 b1.metric("Feels like ❕❗", f"{get_feel()}")
 b2.metric("Highest temperature 🥵", f"{get_temp_max()}")
 b3.metric("Lowest temperature 🥶", f"{get_temp_min()}")
+b4.metric("Visibility 😵", f"{get_visibility()}")
 
 c1, c2 = st.columns((7,3))
 
--- app_funcs.py
+++ app_funcs.py
@@ -2,10 +2,10 @@ import requests
 import json
 import streamlit as st
 
-api_key = st.secrets["API_Key"]
+openweather_api_key = st.secrets["OPENWEATHER_API_Key"]
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
 city_name = "Bangalore"
-complete_url = base_url + "appid=" + api_key + "&q=" + city_name + "&units=metric"
+complete_url = base_url + "appid=" + openweather_api_key + "&q=" + city_name + "&units=metric"
 response = requests.get(complete_url)
 x = response.json()
 
@@ -18,6 +18,7 @@ min_temp = round(y["temp_min"])
 humidity = y["humidity"]
 pressure = y["pressure"]
 feels = y["feels_like"]
+visibility = x["visibility"]
 previous_temp = 23
 
 def temp_difference():
@@ -53,3 +54,15 @@ def get_pressure():
 
 def get_feel():
     return(str(feels)+"°C")
+
+def get_visibility():
+    return(str(visibility/1000)+" km")
+
+def get_aqi():
+    aqi_api_key = st.secrets["AQI_API_Key"]
+    aqi_base_url = "https://api.waqi.info/feed/here/?token="
+    response = requests.get(aqi_base_url + aqi_api_key)
+    aqi_json_output = response.json()
+
+    city_aqi_current = aqi_json_output["data"]["aqi"]
+    return(str(city_aqi_current))
